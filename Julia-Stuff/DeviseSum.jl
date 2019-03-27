d096b39c5e5993afb3673989048080ebba9d4519
Michael Valentino <michael.valentino92@gmail.com>
2019-03-27 12:17:29 +0000

    Add files via upload

--- Julia-Stuff/DeviseSum.jl
+++ Julia-Stuff/DeviseSum.jl
@@ -1,34 +1,31 @@
 #Does stuff!
-function deviseSum(target::Int64,arr::Array{Int64,1})
-	if target == 0 return "" end
-	if length(arr) == 0 return "X" end
-	if !ismissing(table[target]) return table[target] end
-	tail = deepcopy(arr)
-	if length(tail) == 1 tail = Array{Int64,1}()
-	else pop!(tail) end
-	for i in arr
-		upper = Int64(trunc(target/i))
+function deviseSum(target::Int64,arr::Array{Int64,1},index::Int64,len::Int64)
+	if target == 0 return "" end #Hit target, return empty
+	if index > len return "X" end #Ran out of numbers, failure X
+	if !ismissing(table[target]) return table[target] end #Seen it, return
+	for i = index:len
+		n = arr[i]
+		upper = Int64(trunc(target/n))
 		for k = upper:-1:1
-			table[i*k] = "$i*$k"
-			val = string(table[i*k]," + ",deviseSum(target - i*k,tail))
-			if ismissing(table[target]) && val[end] != 'X' 
-				table[target] = val
-				break
-			end
-		end
-		if !ismissing(table[target]) break end
-		val = string("",deviseSum(target,tail))
-		if ismissing(table[target]) && val[end] != 'X' 
-			table[target] = val
-			break
+			table[n*k] = "$n*$k"
+			val = string(table[n*k]," + ",deviseSum(target - n*k,arr,index+1,len))
+			if ismissing(table[target]) #No value here?
+				     if val[end] != 'X' #Hey, it wasn't a failure!
+					     table[target] = val #Update value!
+					     return val #Return it!
+				     end
+			else return table[target] end #Oh we found it!
 		end
 	end
-	if ismissing(table[target]) return "X"
-	else return table[target] end
+	return "X"
 end
 
 #Stuff being done
-n = 6584
-nums = [87,161,213]
-table = Array{Union{String,Missing},1}(missing,n)
-println("$(deviseSum(n,nums)) = $n")
+n = rand(2:10000,1)[1]
+l = 3
+x = div(n,l*10)
+if x > 2*l
+	nums = rand(2:div(n,l*10),l)
+	table = Array{Union{String,Missing},1}(missing,n)
+	println("$(deviseSum(n,nums,1,l)) = $n")
+end
