148ff377870bbd9fdadfd80b5e167c71746258e1
Michael Valentino <michael.valentino92@gmail.com>
2019-06-12 20:12:51 -0400

    Add files via upload

--- Julia-Stuff/ShortestPath.jl
+++ Julia-Stuff/ShortestPath.jl
@@ -11,33 +11,26 @@ end
 #Creates the graph from user input
 function createGraph()
 	retval = Dict{String,Array{Tuple{String,Float64},1}}()
-	#print("How many nodes will be on the graph?: ")
+	print("How many nodes will be on the graph?: ")
 	size = parse(Int64,readline())
+	UnVisited = Array{Node,1}(undef,size)
 	for i in 1:size
-	#	print("Enter new node: ")
+		print("Enter new node: ")
 		k = readline()
+		UnVisited[i] = Node(k,Inf,nothing,nothing)
 		v = Array{Tuple{String,Float64},1}()
-	#	println("Please enter adjacent nodes and their distance in the format \"node distance\".")
-	#	println("Input \"stop\" when you are done.")
-	#	print("node distance: ")
+		println("Please enter adjacent nodes and their distance in the format \"node distance\".")
+		println("Input \"stop\" when you are done.")
+		print("node distance: ")
 		next = split(readline())
 		while next[1] != "stop"
 			push!(v,(next[1],parse(Float64,next[2])))
-	#		print("node distance: ")
+			print("node distance: ")
 			next = split(readline())
 		end
 		retval[k] = v
 	end
-	return retval
-end
-
-#Check if on closed list
-function inClosed(current::Node, dest::(Union{Node,Nothing}), target::String)
-	while current != dest
-		if current.node == target return true end
-		current = current.child
-	end
-	return false
+	return (retval,UnVisited)
 end
 
 #Link the solution backwards
@@ -65,58 +58,64 @@ function printSol(root::Node)
 	println("Path cost: $cost")
 end
 
-#Init, ask for start and destination and solve problem
-graph = createGraph()
-#print("Enter starting node: ")
-start = readline()
-#print("Enter destination node: ")
-dest = readline()
-
-#Init updated solution node, and declare root node with 3 pointers to represent open and closed list
-sol = nothing
-root = Node(start,0,nothing,nothing)
-current = root
-builder = root
-
-#While current does not equal nothing do the follow
-#1) Create all the children
-#2) If child exists on closed list (represented by root -> current), dont add
-#3) If child exists of open list (represented by current -> builder):
-#   - Replace distance and parent if new distance to this node is shorter
-#   - Otherwise, dont add
-#4) If destination node is reached: update sol and exit
-#5) Current = nothing, were done
-while current != nothing
-	global root,current,builder,sol
-	if current.node == dest 
-		current.child = nothing #Kill potential children
-		sol = current #Update with solution and break
-		break
+#Skip if here already
+function inVisited(list::Array{Node,1},target::String)
+	for i in list
+		if i.node == target return true end
 	end
-	children = graph[current.node]
-	for cdl in children
-		if inClosed(root,current.child,cdl[1]) continue end #If we visited here, continue
-		iter = current.child
-		existed = false
-		while iter != builder.child #Check open list, update nodes if we have better
-			if iter.node == cdl[1]
-				existed = true
-				if current.cost + cdl[2] < iter.cost
-					iter.cost = current.cost + cdl[2]
-					iter.parent = current
-				end
+	return false
+end
+
+#Get min of current stuff
+function upDateCost(num,v,list::Array{Node,1},current)
+	for i in 1:length(list)
+		if list[i].node == v
+			if num < list[i].cost
+				list[i].cost = num
+				list[i].parent = current
 				break
 			end
-			iter = iter.child
-		end
-		if !existed #if it wasn't on open list, then add it
-			builder.child = Node(cdl[1],current.cost + cdl[2], current,nothing)
-			builder = builder.child
 		end
 	end
-	current = current.child #Visit next node
+end
+
+#Init, ask for start and destination and solve problem
+(graph,UnVisited) = createGraph()
+print("Enter starting node: ")
+start = readline()
+print("Enter destination node: ")
+dest = readline()
+sol = nothing
+
+#Init updated solution node, and declare root node with 3 pointers to represent open and closed list
+Visited = Array{Node,1}(undef,0)
+for i in 1:length(UnVisited)
+	if UnVisited[i].node == start
+		UnVisited[i].cost = 0
+		break
+	end
+end
+
+#Solves
+while length(UnVisited) > 0
+	global UnVisited,Visited,graph,sol
+	sort!(UnVisited, by = val -> val.cost)
+	currentNode = UnVisited[1]
+	if currentNode.node == dest
+		sol = currentNode
+		break
+	end
+	for i in graph[currentNode.node]
+		println(i)
+		if inVisited(Visited,i[1]) continue end
+		costToNeighbor = i[2]
+		costToCurrent = currentNode.cost
+		upDateCost(costToCurrent + costToNeighbor,i[1],UnVisited,currentNode)
+	end
+	UnVisited = UnVisited[2:end]
+	push!(Visited,currentNode)
 end
 
 #Print results
-#println("\n\n\n\n\n\n")
+println("\n\n\n\n\n\n")
 printSol(linkSolution(sol))
