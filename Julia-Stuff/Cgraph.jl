3312a829e88b51cad66875fb16495412c12d2996
Michael Valentino <valentm3@kean.edu>
2020-05-14 22:26:49 -0400

    Add files via upload

--- Julia-Stuff/Cgraph.jl
+++ Julia-Stuff/Cgraph.jl
@@ -43,53 +43,80 @@ OPS = Dict("+" => +, "-" => -, "*" => *, "/" => /, "^" => ^,
 # The list of binary ops to be split on in order of precedence.
 BINOPS = ["+","-","*","/","^"]
 
-# Fix the negatives in the string
-ops = ['+','-','*','/','^','(',')']
-function fixneg(expr)
-   expr = string("*",expr)
-   expr = string(expr,"*")
-   s = ""
-   i = 2
-   while i < length(expr)
-       if expr[i] == '-' && expr[i-1] in ops
-	   l = 0
-	   r = 0
-	   ss = "(0-"
-	   j = i + 1
-	   while j <= length(expr)
-	       l += expr[j] == '(' ? 1 : 0
-	       r += expr[j] == ')' ? 1 : 0
-	       if expr[j] in ops && l <= r
-		   ss = string(ss,")")
-		   break
-	       else
-		   ss = string(ss,expr[j])
-		   j += 1
-	       end
-	   end
-	   s = string(s,ss)
-	   i = j
-       else
-	   s = string(s,expr[i])
-	   i += 1
-       end
-   end
-   return s
+# Function to replace 6*-2 with 6*(0-2)
+function fixneg(expr::AbstractString)
+
+	# Replace all -- with +
+	expr = replace(expr,"--" => "+")
+
+	# Add a "*" to beginning and end, (so the function wont go out of bounds)
+	expr = string("*",expr)
+	expr = string(expr,"*")
+
+	# Call wrapper, but the * from beginning
+	return fixnegwrapper(expr)[2:end-1]
 end
 
+startops = ['+','*','/','^','(']
+endops = ['+','-','*','/','^',')']
+# Utility function for fixnegs
+function addp(expr::AbstractString)
+	
+	# Find index that an operator first occurs in while l and r are equal
+	i = 1; l = 0; r = 0;
+	while !any(map(x -> x == expr[i],endops)) || l != r
+		l += expr[i] == '(' ? 1 : 0
+		r += expr[i] == ')' ? 1 : 0
+		i += 1
+	end
+
+	# Return new expr
+	front = string(expr[1:i-1],")")
+	whole = string(front,expr[i:end])
+
+	return whole
+end
+
+# Wrapper for fixing negatives in string
+function fixnegwrapper(expr::AbstractString)
+
+	# Base case, if there's no splits return
+	exprs = split(expr,"-",limit=2)
+        if length(exprs) == 1 return exprs[1] end
+           
+	# Otherwise build return value based on split
+        retval = ""
+        if exprs[1][end] in startops
+		retval = string(exprs[1],"(0-")
+		exprs[2] = addp(exprs[2])
+           	retval = string(retval,fixnegwrapper(exprs[2]))
+        else
+		retval = string(exprs[1],"-")
+		retval = string(retval,fixnegwrapper(exprs[2]))
+        end
+	return string(retval)
+end
+
+forwarsdops = ["+","*","^"]
+backwardops = ["-","/"]
 # Function needed to split the expression up to a max of 2 subexpressions around given operator.
 # Doesn't split around parenthesis
-function opsplit(expr::AbstractString,op::String)
+function opsplit(expr::AbstractString,op::String,rev::Bool)
+	
+	# Reverse if needed
+	expr = rev ? reverse(expr) : expr
+
 	l = 0; r = 0; s = "";
 	for i = 1:length(expr)
 		char = expr[i]
 		if string(char) == op && l == r
-			return [string(expr[1:i-1]),string(expr[i+1:end])] 
+			retval = [string(expr[1:i-1]),string(expr[i+1:end])] 
+			return rev ? reverse(map(x -> reverse(x),retval)) : retval
 		end
 		if char == '(' l += 1 end
 		if char == ')' r += 1 end
 	end
-	return [string(expr)]
+	return rev ? [reverse(string(expr))] : [string(expr)]
 end
 
 # Returns the function for this subexpression that contains parenthesis
@@ -108,15 +135,7 @@ function cgraph(expr::AbstractString)
 	expr = filter(x -> !isspace(x),expr)
 
 	# Fix negatives
-	cpyexpr = deepcopy(expr)
-	while true
-		expr = fixneg(expr)
-		if cpyexpr == expr
-			break
-		else
-			cpyexpr = expr
-		end
-	end
+	expr = fixneg(expr)
 
 	# Create root to return
 	root = Node("",nothing,nothing,nothing)
@@ -132,9 +151,12 @@ function cgraphwrapper(expr::AbstractString,root::Node)
 
 	# Begin to find if there is a split.
 	for op in BINOPS
+
+		# See if forward or backward
+		rev = op in backwardops
 		
 		# Seek to split at this operator
-		subexprs = opsplit(expr,op)
+		subexprs = opsplit(expr,op,rev)
 
 		# If there was no split, continue
 		if length(subexprs) == 1 continue end
@@ -151,8 +173,11 @@ function cgraphwrapper(expr::AbstractString,root::Node)
 		setright!(root,right)
 
 		# Call recursively with both these as root with subexprs to mutate roots children
+
+		# If subtraction or division, swap order
 		cgraphwrapper(subexprs[1],left)
 		cgraphwrapper(subexprs[2],right)
+
 
 		# Return to stop
 		return
